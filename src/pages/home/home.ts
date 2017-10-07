f237a7c4ac050fc496bc41ff8489df45ffc48a5d
Henrique Winckler <henriquew@gmail.com>
2017-10-07 11:17:12 -0300

    exemplo de rota traçada

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { GoogleMaps, GoogleMap, LatLng } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, LatLng, GoogleMapsEvent, GoogleMapOptions, CameraPosition } from '@ionic-native/google-maps';
 import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
@@ -23,7 +23,15 @@ export class HomePage {
 
   initMap(){
     this.getLocation().then((resp) => {
-      let latLng = new LatLng(resp.coords.latitude, resp.coords.longitude);
+      //let latLng = new LatLng(resp.coords.latitude, resp.coords.longitude);
+      let latLng = new LatLng(-22.922287, -48.421543);
+
+      let coors : LatLng[];
+      coors = [
+        new LatLng(-22.922287, -48.421543),
+        new LatLng(-22.925121, -48.41461),
+        new LatLng(-22.926569, -48.40753),
+      ];
 
       let mapOptions = {
         'backgroundColor': 'white',
@@ -46,8 +54,53 @@ export class HomePage {
           'bearing': 50
         }
       };
+
+/*      let mOptions: GoogleMapOptions = {
+        mapType: 'MAP_TYPE_TERRAIN',
+        camera: {
+            target: {
+              lat: -22.922287,
+              lng: -48.421543
+            },
+            zoom: 15
+        }
+      }
+
+      let coordinates: [
+        [
+          -22.922287,
+          -48.421543
+        ],
+        [
+          -22.925121,
+          -48.41461
+        ],
+        [
+          -22.926569,
+          -48.40753
+        ]
+      ];
+*/
+      //let coord = coordinates.map(function(point) {
+        //return {lat: point[1], lng: point[0]};
+      //});
+
+
       let element = this.mapElement.nativeElement;
       this.map = this._googleMaps.create(element, mapOptions);
+
+      this.map.one(GoogleMapsEvent.MAP_READY).then(
+        () => {
+          //alert('Map is ready!');
+          
+          this.map.addPolyline({
+             points: coors,
+             'color' : '#AA00FF',
+             'width': 10,
+             'geodesic': true
+          });
+        }
+      );         
 
      }).catch((error) => {
        console.log('Error getting location', error);
